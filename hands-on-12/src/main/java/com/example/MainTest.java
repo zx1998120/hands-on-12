2f0e19ad2445b6218ab7082224ffd93bc53393a6
zx1998120 <73517275+zx1998120@users.noreply.github.com>
2024-04-09 18:23:09 -0400

    Add files via upload

---
+++ hands-on-12/src/main/java/com/example/MainTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertTrue(true); // Simplest test
+    }
+}
